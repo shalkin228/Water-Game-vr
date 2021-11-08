16024bffbff0c255cbfb2db7a1110a48b4254563
shalkin228 <sim.tallinn@gmail.com>
2021-11-08 20:33:36 +0200

    Rising on board system + new crafting menu instantiating system

--- Assets/Scripts/Other/Letter.cs
+++ Assets/Scripts/Other/Letter.cs
@@ -5,5 +5,33 @@ using UnityEngine.InputSystem;
 
 public class Letter : UiInteractBase
 {
+    [SerializeField] private Transform _teleportPos;
     [SerializeField] private InputActionReference _actionReference;
+    private Camera _mainCamera;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+
+        _actionReference.action.performed += ctx => RiseUp();
+    }
+
+    private void RiseUp()
+    {
+        if (_activating == ActivatingType.DeActivating)
+            return;
+
+        var player = FindObjectOfType<WaterMovement>().gameObject;
+        player.GetComponent<CharacterController>().enabled = false;
+        player.transform.position = _teleportPos.position;
+        player.GetComponent<CharacterController>().enabled = true;
+    }
+
+    private void Update()
+    {
+        if(_activating == ActivatingType.Activating)
+        {
+            _canvas.transform.LookAt(_mainCamera.transform);
+        }
+    }
 }
--- Assets/Scripts/Ui/UiInteractBase.cs
+++ Assets/Scripts/Ui/UiInteractBase.cs
@@ -5,32 +5,71 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider))]
 public class UiInteractBase : MonoBehaviour, UIShow
 {
-    [SerializeField] protected Canvas _canvas;
     protected bool _isActive;
+    protected ActivatingType _activating = ActivatingType.DeActivating;
+    protected Canvas _canvas;
+
+    private Vector3 _normalScale;
 
     private void Start()
     {
-        _canvas.gameObject.SetActive(false);
+        _canvas = GetComponentInChildren<Canvas>();
+
+        _normalScale = _canvas.transform.localScale;
+        _canvas.transform.localScale = new Vector3(_canvas.transform.localScale.x,
+            0, _canvas.transform.localScale.z);
     }
 
     private void LateUpdate()
     {
         if (_isActive)
         {
-            _canvas.gameObject.active = true;
-
             _isActive = false;
         }
-        else
+        else if (_activating == ActivatingType.Activating)
         {
-            _canvas.gameObject.active = false;
+            StartCoroutine(DeActivateUI());
         }
     }
 
     public void ActivateUI()
     {
         _isActive = true;
+
+        if (_activating == ActivatingType.DeActivating)
+        {
+            StartCoroutine(ActivateUICorrutine());        
+        }
+
+        _activating = ActivatingType.Activating;
     }
 
-    
+    private IEnumerator ActivateUICorrutine()
+    {
+        while (_isActive)
+        {
+            var currentScale = _canvas.transform.localScale;
+            currentScale = Vector3.Lerp(currentScale, _normalScale,
+                Time.deltaTime * 12);
+            _canvas.transform.localScale = currentScale;
+            yield return null;
+        }
+    }
+
+    private IEnumerator DeActivateUI()
+    {
+        _isActive = false;
+        _activating = ActivatingType.DeActivating;
+
+        while (!_isActive)
+        {
+            var currentScale = _canvas.transform.localScale;
+            currentScale = Vector3.Lerp(currentScale, new Vector3(currentScale.x, 0, currentScale.y),
+                Time.deltaTime * 20);
+            _canvas.transform.localScale = currentScale;
+            yield return null;
+        }
+    }
+
+    protected enum ActivatingType { Activating, DeActivating }
 }
